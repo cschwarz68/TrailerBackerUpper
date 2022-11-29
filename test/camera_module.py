4226576ad03a03a8a8288a9b30a17ed9cc9c20d6
amukundan <adithya-mukundan@uiowa.edu>
2022-11-29 14:44:22 -0600

    camera module

--- test/camera_module.py
+++ test/camera_module.py
@@ -1,24 +1,26 @@
 # import the necessary packages
+import picamera
 from picamera.array import PiRGBArray
-from picamera import PiCamera
 import time
 import cv2
 
-# initialize the camera
-camera = PiCamera()
-
-# allow the camera to warmup
-time.sleep(0.1)
-
+#initialize camera and filename
+camera = picamera.PiCamera()
+rawCapture = PiRGBArray(camera)
+date = "%Y%m%d-%H%M"
+filename = "pi_recording_"+date+".h264"
+#main loop to save video
 def camera_main():
-    # grab a reference to the raw camera capture
-    rawCapture = PiRGBArray(camera)
-    # grab an image from the camera
-    camera.capture(rawCapture, format="bgr")
-    image = rawCapture.array
-    # display the image on screen and wait for a keypress
-    cv2.imshow("Image", image)
-    cv2.waitKey(0)
+    time.sleep(0.1)
+    camera.resolution = (640, 480)
+    camera.start_recording(filename)
+    camera.wait_recording(60)
+    camera.stop_recording()
+#display video
+time.sleep(0.1)
+camera.capture(rawCapture, format="bgr")
+image = rawCapture.array
+cv2.imshow("Image", image)
 
 if __name__ == "__main__":
     camera_main
