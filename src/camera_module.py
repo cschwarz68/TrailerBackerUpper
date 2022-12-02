b73aeaa581fdff85d9beafe81d15895e24244615
Adi M <adithyamukundan@gmail.com>
2022-12-02 15:10:02 -0600

    added image test and video test functions

--- src/camera_module.py
+++ src/camera_module.py
@@ -1,26 +1,30 @@
 # import the necessary packages
 import picamera
 from picamera.array import PiRGBArray
-import time
 import cv2
 
-#initialize camera and filename
+print(cv2.__version__)
+
+# initialize camera and filename
 camera = picamera.PiCamera()
 rawCapture = PiRGBArray(camera)
 date = "%Y%m%d-%H%M"
-filename = "pi_recording_"+date+".h264"
-#main loop to save video
-def camera_main():
-    time.sleep(0.1)
+filename = "pi_recording_" + date + ".h264"
+
+# main loop to save video
+def image_test():
+    # display image
+    camera.capture(rawCapture, format="bgr")
+    image = rawCapture.array
+    cv2.imshow("Image", image)
+
+
+def video_test():
     camera.resolution = (640, 480)
     camera.start_recording(filename)
     camera.wait_recording(60)
     camera.stop_recording()
-#display video
-time.sleep(0.1)
-camera.capture(rawCapture, format="bgr")
-image = rawCapture.array
-cv2.imshow("Image", image)
+
 
 if __name__ == "__main__":
-    camera_main
+    image_test()
