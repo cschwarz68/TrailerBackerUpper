f7142522d874955cc51650cd88d8363c8008cb51
Adi M <adithyamukundan@gmail.com>
2023-01-06 11:19:32 -0600

    working angle measure and image saving

--- src/image_processing_module.py
+++ src/image_processing_module.py
@@ -12,12 +12,14 @@ from skimage.util import img_as_float, crop
 from skimage.measure import regionprops, label
 from skimage.morphology import remove_small_objects
 from PIL import Image
-#import picamera
+import picamera
 import time
+import datetime
 
 # function to modify image
-def imageChanger(img):
-    crop = img[600:1200, 0:1920].copy()
+def image_changer(img):
+    # crop to bottom half of image
+    crop = img[int(img.shape[0] / 2) : int(img.shape[0]), 0 : int(img.shape[1])].copy()
     gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (45, 45), 0)
     ret, binary = cv2.threshold(blur, 150, 255, cv2.THRESH_BINARY)
@@ -26,7 +28,9 @@ def imageChanger(img):
     filtered = remove_small_objects(labelFloat, 16000, 1)
     return filtered
 
-def measureAngles(img):
+
+def measure_angles(img):
+    degrees = []
     for region in regionprops(img):
         majorAxisDegree = region.orientation * (180 / np.pi) + 90
         """print(
@@ -37,30 +41,36 @@ def measureAngles(img):
             + ", "
             + str(region.axis_major_length)
         )"""
-        return majorAxisDegree
+        degrees.append(majorAxisDegree)
+    return degrees
 
-def plotLines(img):
+
+def plot_lines(img):
     for region in regionprops(img):
         minr, minc, maxr, maxc = region.bbox
-        rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr, fill=False, edgecolor='red', linewidth=2)
+        rect = mpatches.Rectangle(
+            (minc, minr),
+            maxc - minc,
+            maxr - minr,
+            fill=False,
+            edgecolor="red",
+            linewidth=2,
+        )
         y0, x0 = region.centroid
         orientation = region.orientation
         x1 = x0 + math.cos(orientation) * 0.5 * region.axis_minor_length
         y1 = y0 - math.sin(orientation) * 0.5 * region.axis_minor_length
         x2 = x0 - math.sin(orientation) * 0.5 * region.axis_major_length
         y2 = y0 - math.cos(orientation) * 0.5 * region.axis_major_length
-        #plt.add_patch(rect)
-        plt.plot((x0, x1), (y0, y1), '-r', linewidth = 2)
-        plt.plot(x0, y0, '.g', markersize = 5)
+        # plt.add_patch(rect)
+        plt.plot((x0, x1), (y0, y1), "-r", linewidth=2)
+        plt.plot(x0, y0, ".g", markersize=5)
         bx = (minc, maxc, maxc, minc, minc)
         by = (minr, minr, maxr, maxr, minr)
-        plt.plot(bx, by, '-b', linewidth = 2)
+        plt.plot(bx, by, "-b", linewidth=2)
     return img
 
-testImage = imageChanger(mpimg.imread("/Users/adimukundan/Documents/GitHub/TrailerBackerUpper/test/assets/ropes.jpg"))
-plt.imshow(plotLines(testImage))
-plt.show()
-'''
+
 # test image processing on image from camera
 def take_picture():
     with picamera.PiCamera() as camera:
@@ -72,33 +82,10 @@ def take_picture():
         camera.capture(image, "bgr")
         image = image.reshape((240, 320, 3))
         """
-        camera.capture("snapshot.jpg", format="jpeg")
-        image = mpimg.imread("/home/nads/Python/TrailerBackerUpper/snapshot.jpg")
-        os.remove("/home/nads/Python/TrailerBackerUpper/snapshot.jpg")
-        plt.imshow(image)
-        plt.show()
+        date = datetime.datetime.now().strftime("%m_%d_%Y-%I_%M_%p")
+        camera.capture(f"captures/snapshot_{date}.jpg", format="jpeg")
+        image = mpimg.imread(f"captures/snapshot_{date}.jpg")
         return image
-
-
-plt.imshow(imageChanger(take_picture()))
-plt.show()
-'''
-
-"""
-finalFig = plt.figure(figsize=(10, 7))
-finalArray = []
-for name in names:
-    image = mpimg.imread(folder_dir + "/" + name)
-    finalArray.append(imageChanger(image))
-    print(name)
-    measureAngles(imageChanger(image))
-for i in range(len(finalArray)):
-    finalFig.add_subplot(rows, columns, i + 1)
-    plt.imshow(finalArray[i])
-    plt.axis("off")
-    plt.title(i)
-plt.show()
-"""
 
 
 def processing_test():
@@ -122,51 +109,21 @@ def processing_test():
         fig.add_subplot(rows, columns, i + 1)
         plt.imshow(files[i])
         plt.axis("off")
-        plt.title(i)
+        plt.title(names[i])
     finalFig = plt.figure(figsize=(10, 7))
     finalArray = []
     for name in names:
         image = mpimg.imread(folder_dir + "/" + name)
-        finalArray.append(imageChanger(image))
-        print(name)
-        measureAngles(imageChanger(image))
+        finalArray.append(image_changer(image))
     for i in range(len(finalArray)):
         finalFig.add_subplot(rows, columns, i + 1)
         plt.imshow(finalArray[i])
+        print(names[i], measure_angles(finalArray[i]))
         plt.axis("off")
-        plt.title(i)
+        plt.title(names[i])
     plt.show()
-    '''
-    blur = cv2.GaussianBlur(image, (5, 5), 0)
-    ret, thresh_binary = cv2.threshold(blur, 127, 255, cv2.THRESH_BINARY)
-    ret, thresh_binary_2 = cv2.threshold(blur, 150, 255, cv2.THRESH_BINARY)
-    ret, otsu = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-    edges = cv2.Canny(blur, 50, 150)
-    lines = cv2.HoughLinesP(edges, 1, np.pi/180, threshold=50, minLineLength=50, maxLineGap=10)
-    line1 = lines[0]
-    line2 = lines[1]
-    points1 = np.array([[line1[0][0], line1[0][1]], [line1[0][2], line1[0][3]]])
-    points2 = np.array([[line2[0][0], line2[0][1]], [line2[0][2], line2[0][3]]])
-    [vx1, vy1, x1, y1] = cv2.fitLine(points1, cv2.DIST_L2, 0, 0.01, 0.01)
-    [vx2, vy2, x2, y2] = cv2.fitLine(points2, cv2.DIST_L2, 0, 0.01, 0.01)
-    angle = np.arctan2(vy1, vx1) - np.arctan2(vy2, vx2)
-    angle = np.rad2deg(angle)
-    print('Angle between lines:', angle)
-    for line in lines:
-        x1, y1, x2, y2 = line[0]
-        cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
-    """
-    cv2.imshow("thresh", thresh_binary)
-    cv2.imshow("thresh2", thresh_binary_2)
-    cv2.imshow("otsu", otsu)
-    cv2.imshow("original", image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    '''
 
 
 if __name__ == "__main__":
-    processing_test()
-    #print('hello')
-    # take_picture()
+    # processing_test()
+    take_picture()
