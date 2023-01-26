b42b116fbf4d2ca5bfbf2698c875c744bd042eec
amukundan <adithya-mukundan@uiowa.edu>
2023-01-25 23:48:51 -0600

    line detection using houghlines

--- src/image_processing_module.py
+++ src/image_processing_module.py
@@ -14,19 +14,23 @@ from PIL import Image as im
 #import picamera
 import time
 import datetime
+
+
 def capture_test():
     folder_dir = "captures"
     for name in os.listdir(folder_dir):
         string = folder_dir+"/"+name
         array = np.fromstring(string)
         print(array.shape)
+
+
 # function to modify image
 def image_changer(img):
     # crop to bottom half of image
     crop = img[int(img.shape[0] / 2) : int(img.shape[0]), 0 : int(img.shape[1])]
     gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
     print(f"max value of gray is {max(gray[0])}")
-    thresh = int(max(gray[0])*0.6)
+    thresh = int(max(gray[0])*0.8)
     # thresh = 150
     blur = cv2.GaussianBlur(gray, (21,21), 0)
     ret, binary = cv2.threshold(blur, thresh, 255, cv2.THRESH_BINARY)
@@ -34,43 +38,101 @@ def image_changer(img):
     labelFloat = label(float)
     filtered = remove_small_objects(labelFloat, 1600, 1)
     return filtered
-def houghlines_changer():
-    image = cv2.imread("test/assets/ropes.jpg", cv2.IMREAD_GRAYSCALE)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    print(image.shape)
-    height = image.shape[0]
-    width = image.shape[1]
-    cropped_image = image[int(image.shape[0] / 2) : int(image.shape[0]), 0 : int(image.shape[1])]
-    gray_image = cv2.cvtColor(cropped_image, cv2.COLOR_RGB2GRAY)
-    blur = cv2.GaussianBlur(gray_image, (9,9), 0)
-    canny_image = cv2.Canny(blur, 100, 200)
-    lines = cv2.HoughLinesP(canny_image,
-                        rho=6,
-                        theta=np.pi/180,
-                        threshold=160,
-                        lines=np.array([]),
-                        minLineLength=40,
-                        maxLineGap=25)
-    image_with_lines = draw_the_lines(cropped_image, lines)
-    plt.imshow(image_with_lines)
-    plt.show()
-def region_of_interest(img, vertices):
-    mask = np.zeros_like(img)
-    channel_count = img.shape[2]
-    match_mask_color = (255,) * channel_count
-    cv2.fillPoly(mask, vertices, match_mask_color)
-    masked_image = cv2.bitwise_and(img, mask)
-    return masked_image
-def draw_the_lines(img, lines):
-    img = np.copy(img)
-    blank_image = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
 
-    for line in lines:
-        for x1, y1, x2, y2 in line:
-            cv2.line(blank_image, (x1,y1), (x2,y2), (0, 255, 0), thickness=10)
 
-    img = cv2.addWeighted(img, 0.8, blank_image, 1, 0.0)
-    return img
+def edge_detector(img):
+    #img = cv2.imread("test/assets/ropes.jpg")
+    #crop = img[int(img.shape[0] / 2) : int(img.shape[0]), 0 : int(img.shape[1])]
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    thresh = int(max(gray[0])*0.8)
+    blur = cv2.GaussianBlur(gray, (21,21), 0)
+    ret, binary = cv2.threshold(blur, thresh, 255, cv2.THRESH_BINARY)
+    edges = cv2.Canny(binary, 200, 400)
+    return edges
+
+
+def region_of_interest(edges):
+    height, width = edges.shape
+    mask = np.zeros_like(edges)
+
+    # only focus bottom half of the screen
+    polygon = np.array([[
+        (0, height * 1 / 2),
+        (width, height * 1 / 2),
+        (width, height),
+        (0, height),
+    ]], np.int32)
+
+    cv2.fillPoly(mask, polygon, 255)
+    cropped_edges = cv2.bitwise_and(edges, mask)
+    return cropped_edges
+
+
+def detect_line_segments(img):
+    rho = 1  # distance precision in pixel, i.e. 1 pixel
+    angle = np.pi / 180  # angular precision in radian, i.e. 1 degree
+    min_threshold = 10  # minimal of votes
+    line_segments = cv2.HoughLinesP(img, rho, angle, min_threshold, 
+                                    np.array([]), minLineLength=8, maxLineGap=4)
+    return line_segments
+    
+def average_slope_intercept(frame, line_segments):
+    lane_lines = []
+    if line_segments is None:
+        print("no lines")
+
+    height, width, _ = frame.shape
+    left_fit = []
+    right_fit = []
+
+    boundary = 1/3
+    left_region_boundary = width * (1 - boundary)  # left lane line segment should be on left 2/3 of the screen
+    right_region_boundary = width * boundary # right lane line segment should be on left 2/3 of the screen
+
+    for line_segment in line_segments:
+        for x1, y1, x2, y2 in line_segment:
+            fit = np.polyfit((x1, x2), (y1, y2), 1)
+            slope = fit[0]
+            intercept = fit[1]
+            if slope < 0:
+                if x1 < left_region_boundary and x2 < left_region_boundary:
+                    left_fit.append((slope, intercept))
+            else:
+                if x1 > right_region_boundary and x2 > right_region_boundary:
+                    right_fit.append((slope, intercept))
+        
+    left_fit_average = np.average(left_fit, axis=0)
+    if len(left_fit) > 0:
+        lane_lines.append(make_points(frame, left_fit_average))
+
+    right_fit_average = np.average(right_fit, axis=0)
+    if len(right_fit) > 0:
+        lane_lines.append(make_points(frame, right_fit_average))
+    
+    return lane_lines
+
+
+def make_points(frame, line):
+    height, width, _ = frame.shape
+    slope, intercept = line
+    y1 = height  # bottom of the frame
+    y2 = int(y1 * 1 / 2)  # make points from middle of the frame down
+
+    # bound the coordinates within the frame
+    x1 = max(-width, min(2 * width, int((y1 - intercept) / slope)))
+    x2 = max(-width, min(2 * width, int((y2 - intercept) / slope)))
+    return [[x1, y1, x2, y2]]
+
+
+def display_lines(frame, lines, line_color=(0, 255, 0), line_width=2):
+    line_image = np.zeros_like(frame)
+    if lines is not None:
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                cv2.line(line_image, (x1, y1), (x2, y2), line_color, line_width)
+    line_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+    return line_image
+
 def measure_angles(img):
     degrees = []
     for region in regionprops(img):
@@ -117,13 +179,28 @@ def plot_lines(img):
         y1 = y0 - math.sin(orientation) * 0.5 * region.axis_minor_length
         x2 = x0 - math.sin(orientation) * 0.5 * region.axis_major_length
         y2 = y0 - math.cos(orientation) * 0.5 * region.axis_major_length
-        # plt.add_patch(rect)
+        img.add_patch(rect)
         plt.plot((x0, x1), (y0, y1), "-r", linewidth=2)
         plt.plot(x0, y0, ".g", markersize=5)
         bx = (minc, maxc, maxc, minc, minc)
         by = (minr, minr, maxr, maxr, minr)
         plt.plot(bx, by, "-b", linewidth=2)
     return img
+
+
+def plot_cv2_lines(img):
+    pass
+
+def plot_test():
+    image = cv2.imread("test/assets/ropes.jpg")
+    image = cv2.resize(image, (640, 480))
+    edges = edge_detector(image)
+    cropped_edges = region_of_interest(edges)
+    line_segments = detect_line_segments(cropped_edges)
+    lane_lines = average_slope_intercept(image, line_segments)
+    line_image = display_lines(image, lane_lines)
+    plt.imshow(line_image)
+    plt.show()
 
 
 # test image processing on image from camera
@@ -185,7 +262,7 @@ def processing_test():
 
 
 if __name__ == "__main__":
-    # processing_test()
+    #processing_test()
     # take_picture()
     # capture_test()
-    houghlines_changer()
+    plot_test()
