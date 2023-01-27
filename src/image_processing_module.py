9699ed73c045f451a043f60adb23076bf7f5cbf7
amukundan <adithya-mukundan@uiowa.edu>
2023-01-26 22:11:19 -0600

    steering output with angle function

--- src/image_processing_module.py
+++ src/image_processing_module.py
@@ -75,7 +75,8 @@ def detect_line_segments(img):
     line_segments = cv2.HoughLinesP(img, rho, angle, min_threshold, 
                                     np.array([]), minLineLength=8, maxLineGap=4)
     return line_segments
-    
+
+
 def average_slope_intercept(frame, line_segments):
     lane_lines = []
     if line_segments is None:
@@ -133,6 +134,44 @@ def display_lines(frame, lines, line_color=(0, 255, 0), line_width=2):
     line_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
     return line_image
 
+
+def compute_steering_angle(frame, lane_lines):
+
+    if len(lane_lines) == 0:
+        return -90
+
+    height, width = frame.shape[0], frame.shape[1]
+    if len(lane_lines) == 1:
+        x1, _, x2, _ = lane_lines[0][0]
+        x_offset = x2 - x1
+    else:
+        _, _, left_x2, _ = lane_lines[0][0]
+        _, _, right_x2, _ = lane_lines[1][0]
+        camera_mid_offset_percent = 0.02 # 0.0 means car pointing to center, -0.03: car is centered to left, +0.03 means car pointing to right
+        mid = int(width / 2 * (1 + camera_mid_offset_percent))
+        x_offset = (left_x2 + right_x2) / 2 - mid
+
+    # find the steering angle, which is angle between navigation direction to end of center line
+    y_offset = int(height / 2)
+
+    angle_to_mid_radian = math.atan(x_offset / y_offset)  # angle (in radian) to center vertical line
+    angle_to_mid_deg = int(angle_to_mid_radian * 180.0 / math.pi)  # angle (in degrees) to center vertical line
+    steering_angle = angle_to_mid_deg + 90  # this is the steering angle needed by picar front wheel
+    return steering_angle
+
+def display_heading_line(frame, steering_angle, line_color=(0, 0, 255), line_width=5, ):
+    heading_image = np.zeros_like(frame)
+    height, width = frame.shape[0], frame.shape[1]
+    steering_angle_radian = steering_angle / 180.0 * math.pi
+    x1 = int(width / 2)
+    y1 = height
+    x2 = int(x1 - height / 2 / math.tan(steering_angle_radian))
+    y2 = int(height / 2)
+
+    cv2.line(heading_image, (x1, y1), (x2, y2), line_color, line_width)
+    heading_image = cv2.addWeighted(frame, 0.8, heading_image, 1, 1)
+
+    return heading_image
 def measure_angles(img):
     degrees = []
     for region in regionprops(img):
@@ -162,35 +201,6 @@ def steering_output(angles):
     return normalized_output
 
 
-def plot_lines(img):
-    for region in regionprops(img):
-        minr, minc, maxr, maxc = region.bbox
-        rect = mpatches.Rectangle(
-            (minc, minr),
-            maxc - minc,
-            maxr - minr,
-            fill=False,
-            edgecolor="red",
-            linewidth=2,
-        )
-        y0, x0 = region.centroid
-        orientation = region.orientation
-        x1 = x0 + math.cos(orientation) * 0.5 * region.axis_minor_length
-        y1 = y0 - math.sin(orientation) * 0.5 * region.axis_minor_length
-        x2 = x0 - math.sin(orientation) * 0.5 * region.axis_major_length
-        y2 = y0 - math.cos(orientation) * 0.5 * region.axis_major_length
-        img.add_patch(rect)
-        plt.plot((x0, x1), (y0, y1), "-r", linewidth=2)
-        plt.plot(x0, y0, ".g", markersize=5)
-        bx = (minc, maxc, maxc, minc, minc)
-        by = (minr, minr, maxr, maxr, minr)
-        plt.plot(bx, by, "-b", linewidth=2)
-    return img
-
-
-def plot_cv2_lines(img):
-    pass
-
 def plot_test():
     image = cv2.imread("test/assets/ropes.jpg")
     image = cv2.resize(image, (640, 480))
@@ -199,26 +209,10 @@ def plot_test():
     line_segments = detect_line_segments(cropped_edges)
     lane_lines = average_slope_intercept(image, line_segments)
     line_image = display_lines(image, lane_lines)
-    plt.imshow(line_image)
+    steering_angle = compute_steering_angle(line_image, lane_lines)
+    steering_image = display_heading_line(line_image, steering_angle)
+    plt.imshow(steering_image)
     plt.show()
-
-
-# test image processing on image from camera
-def take_picture():
-    with picamera.PiCamera() as camera:
-        """
-        camera.resolution = (320, 240)
-        camera.framerate = 24
-        time.sleep(2)
-        image = np.empty((240 * 320 * 3,), dtype=np.uint8)
-        camera.capture(image, "bgr")
-        image = image.reshape((240, 320, 3))
-        """
-        date = datetime.datetime.now().strftime("%m_%d_%Y-%I_%M_%S%p")
-        camera.capture(f"captures/snapshot_{date}.jpg", format="jpeg")
-        image = mpimg.imread(f"captures/snapshot_{date}.jpg")
-        os.remove(f"captures/snapshot_{date}.jpg")
-        return image
 
 
 def processing_test():
@@ -263,6 +257,4 @@ def processing_test():
 
 if __name__ == "__main__":
     #processing_test()
-    # take_picture()
-    # capture_test()
     plot_test()
