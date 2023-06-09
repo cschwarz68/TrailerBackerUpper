2aaee49cdd09d18c288cd40ab8f84bac7fbfbb47
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2023-06-09 09:55:18 -0500

    working on steering rack angles

--- src/drive_module.py
+++ src/drive_module.py
@@ -1,33 +1,34 @@
-'''
-This code is for controlling a robot car with Raspberry Pi using RPi.GPIO library. 
-It defines a class called Drive which represents the car's driving motor and provides methods to control it. 
-The Drive class has methods to set the speed of the car, shift gears (park, drive, reverse), and to stop and cleanup the GPIO pins. 
+"""
+This code is for controlling a robot car with a Raspberry Pi using the RPi.GPIO library. 
+It defines a class called Drive which represents the car's drivetrain and provides methods to control it. 
+The Drive class has methods to set the speed of the car, shift gears (park, drive, reverse), and to stop and clean up the GPIO pins. 
 It uses PWM (pulse width modulation) to control the speed of the motor.
 
-The code also imports a module steer_module for controlling the car's steering servo motor. 
+The code also imports the module steer_module for controlling the car's steering via. servo motor. 
 It defines three test methods to test the car's raw drive command, normalized drive command, and steering and driving simultaneously.
 
 Finally, the code checks if it is being run as the main script and runs the test_steer_and_drive() method which tests both steering and driving 
 at the same time by setting the car to drive at a constant speed and turning the steering servo to the right.
-'''
+"""
 
-#imports
 import RPi.GPIO as GPIO
 import time
+
+# Local Imports
 import steer_module as sr
 
-# GPIO mode should be consistent across all modules
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+# GPIO mode should be consistent across all modules.
+GPIO.setmode(GPIO.BCM) # Pin numbering system: Broadcom SOC
+# GPIO.setwarnings(False) # Disables warnings for pin configuration conflicts.
 
 # Drive control wires
 GPIO.setup(5, GPIO.OUT)
 GPIO.setup(6, GPIO.OUT)
 GPIO.setup(25, GPIO.OUT)
 
-# known calibration settings
+# Known calibration settings.
+#                frequency | max. speed| amount joystick needs to be pushed to drive (prevents drift)
 drive_cal_50 = {"freq": 50, "full": 50, "tol": 1e-3}
-
 
 class Drive:
     def __init__(self, cal=drive_cal_50):
@@ -41,24 +42,22 @@ class Drive:
     def cleanup(self):
         GPIO.cleanup()
 
-    def set(self, dc):
-        self.pwm.ChangeDutyCycle(dc)
+    def set(self, duty_cycle):
+        self.pwm.ChangeDutyCycle(duty_cycle)
 
     def shift(self, gear):
-        """
-        gear 0 = park, 1 = drive, -1 = reverse
-        """
+        # Gears: 0 --> park, 1 --> drive, -1 --> reverse
         if gear == 0:
             GPIO.output(5, GPIO.LOW)
-            GPIO.output(6, GPIO.LOW)
-        elif gear == -1:
-            GPIO.output(5, GPIO.HIGH)
             GPIO.output(6, GPIO.LOW)
         elif gear == 1:
             GPIO.output(5, GPIO.LOW)
             GPIO.output(6, GPIO.HIGH)
+        elif gear == -1:
+            GPIO.output(5, GPIO.HIGH)
+            GPIO.output(6, GPIO.LOW)
         else:
-            raise Exception("unknown gear shift value")
+            raise Exception("Unknown gear shift value.")
 
     def forward(self):
         self.shift(1)
@@ -70,26 +69,25 @@ class Drive:
         self.shift(0)
 
     def drive(self, cmd):
-        """
-        normalized driving. 1 is full speed forward, -1 is full speed reverse
-        """
+        # Normalized driving. 1 is full speed forward, -1 is full speed reverse.
         if abs(cmd) < self.cal["tol"]:
             self.park()
-            dc = 0
+            duty_cycle = 0
         elif cmd > 0:
             self.forward()
             if cmd > 1:
                 cmd = 1
-            dc = 0 + cmd * (self.cal["full"])
+            duty_cycle = 0 + cmd * (self.cal["full"])
         else:
             self.reverse()
             if cmd < -1:
                 cmd = -1
-            dc = 0 - cmd * (self.cal["full"])
-        self.set(dc)
+            duty_cycle = 0 - cmd * (self.cal["full"])
+        self.set(duty_cycle)
 
 
 def constant_speed():
+    print("test: constant_speed")
     drive = Drive()
     while True:
         drive.drive(0.8)
@@ -126,5 +124,5 @@ def test_steer_and_drive():
 
 if __name__ == "__main__":
     # test_cmd()
-    # constant_speed()
-    test_steer_and_drive()
+    constant_speed()
+    #test_steer_and_drive()
--- src/steer_module.py
+++ src/steer_module.py
@@ -11,10 +11,13 @@ GPIO.setup(4, GPIO.OUT)
 #steer_cal_35 = {"freq": 35, "center": 2.4, "right": 4, "left": 1.2}
 steer_cal_35 = {"freq": 35, "center": 4.6, "right": 6.15, "left": 2.2}
 
+
+#I would really like to refactor this to allow passing an angle to some method, and having the wheels turn by that angle
+#also 0 degrees should be the middle like cmon
 class Steer:
     def __init__(self, cal=steer_cal_35):
         self.cal = cal
-        self.pwm = GPIO.PWM(4, cal["freq"])
+        self.pwm = GPIO.PWM(4, cal["freq"]) 
         self.pwm.start(cal["center"])
         self.current_steering_angle = 90
 
@@ -26,6 +29,7 @@ class Steer:
 
     def set(self, dc):
         self.pwm.ChangeDutyCycle(dc)
+
 
     def steer(self, cmd):
         """
@@ -41,6 +45,11 @@ class Steer:
             dc = self.cal["center"] - cmd * (self.cal["left"] - self.cal["center"])
         self.set(dc)
 
+    # id like to make some changes here
+    # namely, according to the test below, you can input steering angles in a range of 0-180, which is a wider range than the steering rack actually turns
+    # after we find out what the true angles of the rack are, id like steering angles to range from [-BOUND, BOUND], where BOUND is the absolute 
+    # value of the turning angle in either direction (45 degrees?). also 0 should correspond with driving straight.
+    # -Chris
     def steer_by_angle(self, angle, scale=1.0):
         cmd = (angle - 90) / 45
         cmd = cmd * scale
@@ -52,7 +61,11 @@ class Steer:
             if cmd < -1:
                 cmd = -1
             dc = self.cal["center"] - cmd * (self.cal["left"] - self.cal["center"])
+        print(dc)
         self.set(dc)
+
+   
+
 
     def trailer_steering_test(self, angle):
         cmd = (angle - 90) / 45
@@ -103,9 +116,12 @@ def test_raw():
 
 def test_by_angle():
     steer = Steer()
-    cmd = "90"
+    cmd = "90" #questionable choice of default pos 
     while cmd != "q":
         steer.steer_by_angle(float(cmd))
+
+        #TODO:
+        # 0-180 is actually a bad angle range. the rack cannot actually turn 90 degrees in each direction.
         cmd = input("Enter steering angle 0 - 180. q to quit: ")
     steer.stop()
     steer.cleanup()
@@ -122,7 +138,8 @@ def test_cmd():
 
 
 if __name__ == "__main__":
-    test_cmd()
+    #test_cmd()
+    test_by_angle()
     #test_raw()
 
 
