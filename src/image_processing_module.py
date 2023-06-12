46e23fa94b8a762c6b604d0cbcd9f09a2ff5a95b
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2023-06-12 13:02:52 -0500

    Changed array dimensions.

--- src/image_processing_module.py
+++ src/image_processing_module.py
@@ -127,11 +127,11 @@ def compute_steering_angle(frame, lane_lines):
 
     height, width = frame.shape[0], frame.shape[1]
     if len(lane_lines) == 1:
-        x1, _, x2, _ = lane_lines[0][0]
+        x1, _, x2, _ = lane_lines[0]
         x_offset = x2 - x1
     else:
-        _, _, left_x2, _ = lane_lines[0][0]
-        _, _, right_x2, _ = lane_lines[1][0]
+        _, _, left_x2, _ = lane_lines[0]
+        _, _, right_x2, _ = lane_lines[1]
         camera_mid_offset_percent = 0.02  # 0.0 means car pointing to center, -0.03: car is centered to left, +0.03 means car pointing to right
         mid = int(width / 2 * (1 + camera_mid_offset_percent))
         x_offset = (left_x2 + right_x2) / 2 - mid
