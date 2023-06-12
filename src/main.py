d5bef827b74c47017fb56f1dc08bacef040c338c
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2023-06-12 10:19:48 -0500

    Remove disfunctional video capture related components.

--- src/main.py
+++ src/main.py
@@ -103,9 +103,6 @@ def manual(events):
     ])
     if pressed["Key"]["BTN_EAST"] == 1:
         done = True
-    elif pressed["Key"]["BTN_SOUTH"] == 1:
-        pass # Bypassing functionality for now.
-        # video_capture()
     elif pressed["Key"]["BTN_NORTH"] == 1:
         transition_mode = Main_Mode.AUTO_FORWARD
         print("Transitioned to auto FORWARD. Press START to init.")
@@ -198,42 +195,14 @@ def main():
         if mode == Main_Mode.MANUAL:
             manual(events)
         elif mode == Main_Mode.AUTO_FORWARD:
+            # Need to move the camera to the front so it can see ahead. Take note of new and former positions!
             auto_forward()
         elif mode == Main_Mode.AUTO_REVERSE:
             auto_reverse()
 
     steer.stop()
+    stream.stop()
     steer.cleanup()
-
-    # Currently bypassed functionality.
-    # stream.stop()
-    # camera.stop()
-    # out.release()
-
-def video_capture():
-    global stream, steer
-    # Processing and capture test.
-    image = stream.capture()
-    edges = ip.edge_detector(image)
-    cropped_edges = ip.region_of_interest(edges)
-    line_segments = ip.detect_line_segments(cropped_edges)
-    lane_lines = ip.average_slope_intercept(image, line_segments)
-    # num_lanes = len(lane_lines)
-    line_image = ip.display_lines(image, lane_lines)
-    # Recording.
-    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-    out = cv2.VideoWriter(
-        "test_recording.mp4", fourcc, 20.0, (320, 240)
-    )
-    while True:
-        frame = cv2.cvtColor(
-            np.array(line_image), cv2.COLOR_GRAY2BGR
-        )
-        out.write(frame)
-        steering_angle = ip.compute_steering_angle(
-            line_image, lane_lines
-        )
-        steer.steer_by_angle(steering_angle)
 
 if __name__ == "__main__":
     main()
