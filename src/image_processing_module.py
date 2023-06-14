f2ba765cda7ae7092d689ae6bdb23a2efce5cbb8
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2023-06-14 10:56:57 -0500

    Continued refactoring quick capture module. Added better debug output and tests.

--- src/image_processing_module.py
+++ src/image_processing_module.py
@@ -38,8 +38,8 @@ def region_of_interest(edges: cv2.Mat) -> cv2.Mat:
     polygon = np.array(
         [
             [
-                (0, height * 1 / 2), 
-                (width, height * 1 / 2), 
+                (0, height / 2), 
+                (width, height / 2), 
                 (width, height), 
                 (0, height)
             ]
@@ -120,6 +120,7 @@ def average_slope_intercept(frame: cv2.Mat, line_segments: np.ndarray) -> list[t
     if len(right_fit) > 0:
         lane_lines.append(make_points(frame, right_fit_average))
 
+    # Because lane_lines will only ever have two values, we could return this as a tuple, but that would require unnecessary refactoring.
     return lane_lines
 
 # Use lane lines to predict the steering angle in degrees.
@@ -129,7 +130,7 @@ def compute_steering_angle(frame: cv2.Mat, lane_lines: list[tuple[int, int, int,
         # Continue straight if no lines are present...
         return Drive_Params.TURN_STRAIGHT
 
-    height, width = frame.shape[0], frame.shape[1]
+    height, width, _ = frame.shape
     if len(lane_lines) == 1:
         # In the event only a left lane or right lane was detected, 
         # we calculate the x-offset based on the difference between the extrapolated x coordinates.
@@ -162,180 +163,65 @@ def compute_steering_angle(frame: cv2.Mat, lane_lines: list[tuple[int, int, int,
     steering_angle = int(angle_to_mid_deg + Drive_Params.TURN_STRAIGHT)
     return steering_angle
 
-# Everything beneath this comment is for testing.
+"""
 
-# Returns a black image with lines drawn on it.
-def display_lines(frame: cv2.Mat, lines: list[tuple[int, int, int, int]], line_color=(255, 255, 255), line_width=2) -> cv2.Mat:
-    line_image = np.zeros_like(frame) # Create black image with the same dimensions.
+Everything beneath this comment is for testing.
+Run `quick_capture_module.py` for unit tests.
+
+"""
+
+# Returns a black image with dimensions identical to that of the given image.
+def zero_image(frame: cv2.Mat) -> cv2.Mat:
+    return np.zeros_like(frame)
+
+# Combines images together, where weight of each image can be adjusted.
+# Images later in the list take z-axis priority.
+def combine_images(pairs: list[tuple[cv2.Mat, float]]) -> cv2.Mat:
+    # Throw exception if no images are provided.
+    base = zero_image(pairs[0][0])
+    for image, weight in pairs:
+        # The last parameter is gamma, and is for adjusting the overall brightness.
+        base = cv2.addWeighted(base, 1, image, weight, 0)
+    return base
+
+# Adds lines to image. Color is in RGB format.
+def display_lines(img: cv2.Mat, lines: list[tuple[int, int, int, int]], line_color=(255, 255, 255), line_width=2) -> cv2.Mat:
+    line_image = img.copy()
     if lines is None:
         return line_image
     for line in lines:
         x1, y1, x2, y2 = line
         cv2.line(line_image, (x1, y1), (x2, y2), line_color, line_width)
-    # Might be useful for overlaying the raw image.
-    # line_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
     return line_image
 
-# Makes a black image containing lane lines and the calculated path. 
-def display_lanes_and_path(img: cv2.Mat):
+# Returns the steering angle and lanes.
+# Should be identical to the steps in main, but separate for testing.
+def steering_info(img: cv2.Mat) -> tuple[int, list[tuple[int, int, int, int]]]:
     edges = edge_detector(img)
     cropped_edges = region_of_interest(edges)
     line_segments = detect_line_segments(cropped_edges)
     lane_lines = average_slope_intercept(img, line_segments)
     steering_angle_deg = compute_steering_angle(img, lane_lines)
+    return (steering_angle_deg, lane_lines)
+
+# Makes a black image containing lane lines and the calculated path. 
+def display_lanes_and_path(img: cv2.Mat, steering_angle_deg: int, lane_lines: list[tuple[int, int, int, int]]) -> cv2.Mat:
     height, width = img.shape[0], img.shape[1]
     steering_angle_radian = math.radians(steering_angle_deg)
+
+    # Calculations for the center path.
     x1 = int(width / 2)
     y1 = height
     x2 = int(x1 - height / 2 / math.tan(steering_angle_radian))
     y2 = int(height / 2)
 
-    final_image = display_lines(img, lane_lines)
-    cv2.line(final_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    # final_image = display_lines(img, lane_lines)
+    # cv2.line(final_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    final_image = combine_images([(img, 0.25)]) # Reduce opacity of base image.
+    final_image = display_lines(final_image, lane_lines)
+    final_image = display_lines(final_image, [(x1, y1, x2, y2)], (0, 255, 0))
+
+    final_image = cv2.putText(final_image, "Steering Angle from Straight: " + str(steering_angle_deg - Drive_Params.TURN_STRAIGHT), 
+                              (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
 
     return final_image
-
-# R
-def lane_detection(img):
-    """
-    @brief plot lane lines and steering path on frame
-    
-    @param img(numpy array): A numpy array representation of original image
-    
-    @return final_image(numpy array): numpy array of img with lane lines and steering path plotted"""
-    edges = edge_detector(img)
-    cropped_edges = region_of_interest(edges)
-    line_segments = detect_line_segments(cropped_edges)
-    lane_lines = average_slope_intercept(img, line_segments)
-    line_image = display_lines(img, lane_lines)
-    steering_angle = compute_steering_angle(line_image, lane_lines)
-    final_image = display_heading_line(line_image, steering_angle)
-    return final_image
-
-# Everything beneath this comment has not been refactored.
-
-def steering_output(angles):
-    normalized_output = 0
-    if len(angles) == 2:
-        normalized_output = (180 - (angles[0] + angles[1])) / 180
-    elif len(angles) == 1:
-        if angles[0] >= 5:
-            normalized_output = (180 - angles[0]) / 180
-        else:
-            # throw stop flag as end has been reached
-            pass
-    return normalized_output
-
-
-def get_steering_angle(img):
-    """
-    @brief get steering angle to keep car in middle of lane
-
-    @param img(numpy array): A numpy array representation of original image
-
-    @return steering_angle(int): steering angle to keep car in middle of lane
-    """
-    edges = edge_detector(img)
-    cropped = region_of_interest(edges)
-    line_segments = detect_line_segments(cropped)
-    lane_lines = average_slope_intercept(img, line_segments)
-    line_image = display_lines(img, lane_lines)
-    steering_angle = compute_steering_angle(line_image, lane_lines)
-    return steering_angle
-
-
-def get_reds(img):
-    """
-    @brief filter image for red color to detect tape on trailer
-
-    @param img(numpy array): A numpy array representation of image
-
-    @return mask(Mat): image filtered for red
-    """
-    invert = ~img
-    hsv = cv2.cvtColor(invert, cv2.COLOR_BGR2HSV)
-    lower_cyan = np.array([80, 150, 40])
-    upper_cyan = np.array([100, 255, 255])
-    mask = cv2.inRange(hsv, lower_cyan, upper_cyan)
-    #res = cv2.bitwise_and(img, img, mask= mask)
-    return mask
-
-
-def get_angle_image(img):
-    """
-    @brief calculate angle of largest shape in image and calculate
-    
-    @param img(Mat): matrix representation of image
-    
-    @return image(array): image with angle of largest shape plotted"""
-    contours = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours = contours[0] if len(contours) == 2 else contours[1]
-    big_contour = max(contours, key=cv2.contourArea)
-    M = cv2.moments(big_contour)
-    cx = int(M["m10"] / M["m00"])
-    cy = int(M["m01"] / M["m00"]) 
-    origin_x, origin_y = int(img.shape[1]/2), img.shape[0]
-    angle = math.atan2(origin_y - cy, origin_x - cx)
-    angle = abs(math.floor(math.degrees(angle)))
-    image = cv2.line(img, (origin_x, origin_y), (cx, cy), color=(255, 255, 255), thickness=5)
-    image = cv2.putText(image, str(angle), (cx,cy), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,255), 2)
-    return image
-
-
-def get_red_angle(img):
-    """
-    @brief calculate angle of largest shape detected in image
-    
-    @param img(Mat): matrix representation of image
-    
-    @return angle(int): angle of shape"""
-    contours = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours = contours[0] if len(contours) == 2 else contours[1]
-    big_contour = max(contours, key=cv2.contourArea)
-    M = cv2.moments(big_contour)
-    cx = int(M["m10"] / M["m00"])
-    cy = int(M["m01"] / M["m00"]) 
-    origin_x, origin_y = int(img.shape[1]/2), img.shape[0]
-    angle = math.atan2(origin_y - cy, origin_x - cx)
-    angle = abs(math.floor(math.degrees(angle)))
-    return angle
-
-
-def display_reds_and_lane(img):
-    """
-    @brief detect and plot lane lines and line of red shape
-    
-    @param img(Mat): matrix representation of image
-    
-    @return line_image(numpy array): image with lane lines and red shape angle line
-    """
-    red = get_reds(img)
-    # if no red detected use original image
-    try:
-        red_angle_image = get_angle_image(red)
-    except:
-        red_angle_image = img
-    edges = edge_detector(img)
-    cropped_edges = region_of_interest(edges)
-    line_segments = detect_line_segments(cropped_edges)
-    lane_lines = average_slope_intercept(img, line_segments)
-    line_image = display_lines(red_angle_image, lane_lines)
-    return line_image
-
-
-def red_and_lane_angle(img):
-    """
-    @brief calculate and return angle of red shape and steering angle from lane lines
-    
-    @param img(Mat): matrix representation of image
-    
-    @return red_angle, steering_angle (int, int): angle of red shape and steering angle from lane lines
-    """
-    red = get_reds(img)
-    #if no red detected default to 90 degrees
-    try:
-        red_angle = get_red_angle(red)
-    except:
-        red_angle = 90
-    steering_angle = get_steering_angle(img)
-    return red_angle, steering_angle
--- src/quick_capture_module.py
+++ src/quick_capture_module.py
@@ -7,6 +7,8 @@ The `stream_capture` method is similar, but captures and returns a continuous st
 The main script creates an instance of the StreamCamera class, captures an image in a loop, applies some image processing techniques from the image_processing_module, 
 displays the resulting image using the OpenCV library, and exits when the 'q' key is pressed.
 Finally, it calls the stop method to stop the camera preview and close the camera.
+
+Note: If the images change to fast or an extreme angle is detected, numpy will emit a RuntimeWarning. This is okay.
 """
 
 # Package Imports
@@ -17,7 +19,7 @@ import cv2
 
 # Local Imports
 import image_processing_module as ip
-# from constants import Camera_Settings # Uncomment for additional configurations.
+from constants import Drive_Params #, Camera_Settings # Uncomment for additional configurations.
 class StreamCamera:
     def __init__(self):
         self.camera = Picamera2()
@@ -37,7 +39,11 @@ class StreamCamera:
         array = cv2.cvtColor(array, cv2.COLOR_BGR2RGB)
         return array
 
-    # Everything beneath this comment is for testing.
+    """
+
+    Everything beneath this comment is for testing.
+
+    """
 
     # Tests if the camera is working by taking an image and saving it to disk.
     def test(self):
@@ -59,16 +65,26 @@ class StreamCamera:
 # Unit Test
 if __name__ == "__main__":
     camera = StreamCamera()
+    debug_output = []
 
     while True:
         image = camera.capture()
-        image = ip.display_lanes_and_path(image)
+        steering_angle_deg, lane_lines = ip.steering_info(image)
+        image = ip.display_lanes_and_path(image, steering_angle_deg, lane_lines)
         cv2.imshow("Quick Capture Module Unit Test - Auto Forward Lanes and Path", image)
+        lane_lines_len = len(lane_lines)
 
-        # Red stuff for later.
-        # image = ip.display_reds_and_lane(image)
+        # Print debugging while the camera is running decreases performance likely due to stdout buffering.
+        # Print the entire thing on exit.
+        debug_output.append("Angle: " + str(steering_angle_deg - Drive_Params.TURN_STRAIGHT) + "\t" + 
+            "No Lanes" if lane_lines_len == 0 
+            else "One Lane: " + str(lane_lines[0]) if lane_lines_len == 1 
+            else "Left Lane: " + str(lane_lines[0]) + " | Right Lane: " + str(lane_lines[1])
+        )
 
         # Exit upon pressing (q). Make sure the preview window is focused.
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
     camera.stop()
+    for msg in debug_output:
+        print(msg)
