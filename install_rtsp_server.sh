e971d0da6bacd4cdbf3c0df90011e9cac4ffafe2
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2023-06-15 11:51:54 -0500

    Remove archive file.

--- install_rtsp_server.sh
+++ install_rtsp_server.sh
@@ -1,3 +1,4 @@
 clear
 wget https://github.com/bluenviron/mediamtx/releases/download/v0.23.5/mediamtx_v0.23.5_linux_arm64v8.tar.gz
 tar -xf mediamtx_v0.23.5_linux_arm64v8.tar.gz
+rm mediamtx_v0.23.5_linux_arm64v8.tar.gz
