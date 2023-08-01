a2f72319df89a03fb9e6c6a1dc852f64bf07e2ed
Christopher Nair <cnair@uiowa.edu>
2023-08-01 09:29:53 -0500

    combined motor and gpio

--- src/gpio.py
+++ src/gpio.py
@@ -1,26 +1,103 @@
 """
-To use the pigpio daemon.
+This module provides abstractions of servos and DC motors.
 """
 
-import pigpio as io
+from pigpio import pi, OUTPUT, LOW, HIGH
+import time
+import os
 
-class IO(io.pi):
+os.system("systemctl start pigpiod")
+time.sleep(1) # Daemon needs a second to warm up.
+
+io = pi()
+io.set_PWM_range(9, 100) # Makes it so 50 = 50% duty cycle.
+
+FREQ = 50 # Default PWM frequency.
+
+class DCMotor:
     """
-    The parent of this class must be instantiated to interface with pigpio daemon. This is done by the instance of IO in motor.py.
+    DCMotor.
     """
 
-    def set_low(self, pin):
-        super().write(pin, 0)
+    STOP    = 0
+    FORWARD = 1
+    REVERSE = -1
 
-    def set_high(self, pin):
-        super().write(pin, 1)
+    def __init__(self, power_pin: int, forward_pin: int, reverse_pin: int):
+        self.power_pin = power_pin 
+        self.forward_pin = forward_pin
+        self.reverse_pin = reverse_pin 
 
-    def set_output(self, pin):
-        super().set_mode(pin, io.OUTPUT)
+        io.set_mode(power_pin, OUTPUT)
+        io.set_mode(forward_pin, OUTPUT)
+        io.set_mode(reverse_pin, OUTPUT)
 
-    def set_input(self, pin):
-        super().set_mode(pin, io.INPUT)
+        io.set_PWM_frequency(power_pin, FREQ)
+        io.set_PWM_frequency(forward_pin, FREQ)
+        io.set_PWM_frequency(reverse_pin, FREQ)
 
-# Should I get rid of this file? Sort of just bloats the file structure and
-# The only benefit is nicer (arguably) names for a couple functions... I don't think I even use `set_input()`
-# Counter argument: if it ain't broke don't fix it.
+        
+
+    def forward(self):
+        io.write(self.reverse_pin, LOW)
+        io.write(self.forward_pin, HIGH)
+
+    def reverse(self):
+        io.write(self.forward_pin, LOW)
+        io.write(self.reverse_pin, HIGH)
+
+    def stop_rotation(self):
+        io.write(self.forward_pin, LOW)
+        io.write(self.reverse_pin, LOW)
+
+    def stop(self):
+        io.set_PWM_dutycycle(self.power_pin, 0)
+
+    def set_power(self, duty_cycle: int):
+        io.set_PWM_dutycycle(self.power_pin, duty_cycle)
+
+class Servo:
+    """
+    Servo.
+    """
+
+    def __init__(self, pin:int, freq: int = FREQ):
+        io.set_mode(pin, OUTPUT)
+        io.set_PWM_frequency(pin, freq)
+        self.pin: int = pin
+
+    def stop(self):
+        self.set_pulse_width(0)
+
+
+    """
+    0 -> full counterclockwise
+    60 -> center
+    120 -> full clockwise
+    """
+    def set_angle(self, theta: float):
+
+        # Equation derived from relating desired angles to required pulse width.
+        # See: https://en.wikipedia.org/wiki/Servo_control#Pulse_duration
+        # See: https://en.wikipedia.org/wiki/Pulse-width_modulation
+        pulse_width = theta * 25 / 3 + 1000
+
+        self.set_pulse_width(pulse_width)
+
+
+    """
+    0    -> off
+    1000 -> full counterclockwise
+    1500 -> center
+    2000 -> full clockwise
+    """
+    def set_pulse_width(self, width: int):
+        io.set_servo_pulsewidth(self.pin, width)
+
+
+def cleanup():
+    """Releases GPIO resources and stops daemon"""
+    print("Releasing GPIO resources... ", end="")
+    io.stop()
+    os.system("systemctl stop pigpiod")
+    print("DONE")
