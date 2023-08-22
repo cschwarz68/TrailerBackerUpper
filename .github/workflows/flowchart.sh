43c16c30441915f3a9449d6d6bb7f547beeb76c2
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2023-08-21 19:39:03 -0500

    Add nice flowchart CI/CD. (#2)
    Using [this project](https://github.com/cdfmlr/pyflowchart) to generate web pages with flowcharts.
    The GitHub Actions workflow installs this with pip and then launches a script which iterates through all the Python files and generates a page for each one - placed under `docs/flowcharts`.
    This will be a squash merge.
    
    From commit: Assume changes pass through staging before being merged into main.
    The GitHub Actions page build runs in parallel with other actions, so the flowcharts will have to be computed beforehand anyways.

---
+++ .github/workflows/flowchart.sh
@@ -0,0 +1,14 @@
+echo "Starting transpilation."
+cd src
+
+FILES=$(find . -type f -name "*.py" | cut -c 3-)
+echo $FILES
+
+for FILE in $FILES; do
+    if [ -f $FILE ]; then
+        OUTPUT="../docs/flowcharts/$(echo $FILE | tr "/." "_").html"
+        touch $OUTPUT
+        echo "Transpiling ${FILE} to ${OUTPUT}."
+        python3 -m pyflowchart $FILE -o $OUTPUT
+    fi
+done
